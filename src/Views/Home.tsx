a51eccad815da41d33a7ce6bf16d1e34f68ea0a9
Antonio Ochoa <antonhub00@gmail.com>
2021-11-04 17:39:41 -0600

    ADD: Home view

---
+++ src/Views/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from 'native-base';
+import TopBarComponent from '../Components/TopBarComponent';
+
+const Home: React.FC = () => {
+  return (
+    <Box>
+      <TopBarComponent />
+    </Box>
+  );
+};
+
+export default Home;
