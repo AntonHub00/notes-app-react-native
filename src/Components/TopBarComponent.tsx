a4aaed9b42525a101ae838c93fcc14232bc54e90
Antonio Ochoa <antonhub00@gmail.com>
2021-11-05 01:02:02 -0600

    FIX: Contrast issues in top bar component and icon button extracted

--- src/Components/TopBarComponent.tsx
+++ src/Components/TopBarComponent.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
-import { HamburgerIcon, HStack, IconButton, Input } from 'native-base';
+import { Box, HamburgerIcon, HStack, IconButton, Input } from 'native-base';
 import AvatarComponent from './AvatarComponent';
+
+const CustomIconButtonComponent: React.FC<{ icon: JSX.Element }> = ({
+  icon,
+}) => (
+  <IconButton
+    borderRadius="100"
+    _pressed={{ bgColor: '#00838f80' }}
+    icon={icon}
+  />
+);
 
 const TopBarComponent: React.FC = () => {
   return (
-    <HStack bg="#00BCD4" h="12" space="2" p="3" alignItems="center">
-      <Input
-        flex={1}
-        h="10"
-        placeholder="Search Notes"
-        bg="#fff"
-        borderRadius="100"
-        InputLeftElement={
-          <IconButton icon={<HamburgerIcon size="sm" color="black" />} />
-        }
-        InputRightElement={
-          <IconButton
-            icon={
-              <AvatarComponent
-                size="xs"
-                uri="https://pbs.twimg.com/profile_images/1177303899243343872/B0sUJIH0_400x400.jpg"
-              />
-            }
-          />
-        }
-      />
+    <HStack bg="#00838f" h="12" space="2" p="3" alignItems="center">
+      <Box flex={1} h="10" bg="white" borderRadius="100">
+        <Input
+          borderWidth="0"
+          selectionColor="#99999980"
+          placeholder="Search Notes"
+          InputLeftElement={
+            <CustomIconButtonComponent
+              icon={<HamburgerIcon size="sm" color="black" />}
+            />
+          }
+          InputRightElement={
+            <CustomIconButtonComponent
+              icon={
+                <AvatarComponent
+                  size="xs"
+                  uri="https://pbs.twimg.com/profile_images/1177303899243343872/B0sUJIH0_400x400.jpg"
+                />
+              }
+            />
+          }
+        />
+      </Box>
     </HStack>
   );
 };
