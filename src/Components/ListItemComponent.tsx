838c68ae9ad51bafe034b32e791fdf36adc5e37c
Antonio Ochoa <antonhub00@gmail.com>
2021-11-05 17:35:16 -0600

    ADD: Style for notes list item pressed

--- src/Components/ListItemComponent.tsx
+++ src/Components/ListItemComponent.tsx
@@ -1,4 +1,4 @@
-import { Text, VStack } from 'native-base';
+import { Pressable, Text, VStack } from 'native-base';
 import React from 'react';
 
 interface Props {
@@ -9,21 +9,30 @@ interface Props {
 
 const ListItemComponent: React.FC<Props> = ({ title, content }) => {
   return (
-    <VStack
-      m="1.5"
-      p="2"
-      space="1"
-      borderRadius="sm"
-      borderWidth="1"
-      borderColor="#aaa"
-    >
-      <Text fontSize="md" fontWeight="bold">
-        title: {title}
-      </Text>
-      <Text fontSize="xs" color="#555">
-        content: {content}
-      </Text>
-    </VStack>
+    <Pressable onPress={() => {}}>
+      {({ isPressed }) => (
+        <VStack
+          m="1.5"
+          p="2"
+          space="1"
+          borderRadius="sm"
+          borderWidth="1"
+          borderColor="#aaa"
+          bgColor={isPressed ? '#00838f' : '#fff'}
+        >
+          <Text
+            fontSize="md"
+            fontWeight="bold"
+            color={isPressed ? '#fff' : '#000'}
+          >
+            title: {title}
+          </Text>
+          <Text fontSize="xs" color={isPressed ? '#fff' : '#555'}>
+            content: {content}
+          </Text>
+        </VStack>
+      )}
+    </Pressable>
   );
 };
 
