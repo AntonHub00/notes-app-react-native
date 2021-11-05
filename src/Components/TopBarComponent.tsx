f1064044103dd90624df03e9e38cd267fbf21460
Antonio Ochoa <antonhub00@gmail.com>
2021-11-05 04:58:24 -0600

    ADD: Action when clicking profile avatar in top bar component

--- src/Components/TopBarComponent.tsx
+++ src/Components/TopBarComponent.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Box, HamburgerIcon, HStack, IconButton, Input } from 'native-base';
 import AvatarComponent from './AvatarComponent';
 
-const CustomIconButtonComponent: React.FC<{ icon: JSX.Element }> = ({
-  icon,
-}) => (
+const CustomIconButtonComponent: React.FC<{
+  icon: JSX.Element;
+  onPressAction?: () => void;
+}> = ({ icon, onPressAction }) => (
   <IconButton
     borderRadius="100"
     _pressed={{ bgColor: '#00838f80' }}
     icon={icon}
+    onPress={onPressAction}
   />
 );
 
-const TopBarComponent: React.FC = () => {
+const TopBarComponent: React.FC<{ onProfileAvatarPress: () => void }> = ({
+  onProfileAvatarPress,
+}) => {
   return (
     <HStack bg="#00838f" h="12" space="2" p="3" alignItems="center">
       <Box flex={1} h="10" bg="white" borderRadius="100">
@@ -33,6 +37,7 @@ const TopBarComponent: React.FC = () => {
                   uri="https://pbs.twimg.com/profile_images/1177303899243343872/B0sUJIH0_400x400.jpg"
                 />
               }
+              onPressAction={onProfileAvatarPress}
             />
           }
         />
