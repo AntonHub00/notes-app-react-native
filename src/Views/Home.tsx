5611d950e05c1987222dc6cf9596448981781c06
Antonio Ochoa <antonhub00@gmail.com>
2021-11-05 01:07:43 -0600

    ADD: Flat list in Home view

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Box } from 'native-base';
+import { Box, FlatList } from 'native-base';
 import TopBarComponent from '../Components/TopBarComponent';
+import ListItemComponent from '../Components/ListItemComponent';
 
 const Home: React.FC = () => {
+  const notes = Array(10)
+    .fill('')
+    .map((_, index) => ({
+      id: index + 1,
+      title: `Note ${index + 1}`,
+      content: `Note ${index + 1} Content`,
+    }));
+
   return (
-    <Box>
+    <Box h="full">
       <TopBarComponent />
+      <FlatList
+        data={notes}
+        renderItem={({ item }) => (
+          <ListItemComponent
+            id={item.id}
+            title={item.title}
+            content={item.content}
+          />
+        )}
+        keyExtractor={item => item.id}
+      />
     </Box>
   );
 };
