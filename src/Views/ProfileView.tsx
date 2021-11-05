3c279e09df8b7be9739befd16a70ccd256e73ca3
Antonio Ochoa <antonhub00@gmail.com>
2021-11-05 03:21:59 -0600

    ADD: Profile view

---
+++ src/Views/ProfileView.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { VStack, ArrowBackIcon, IconButton } from 'native-base';
+import AvatarComponent from '../Components/AvatarComponent';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import BaseButtonComponent from '../Components/BaseButtonComponent';
+import FormControlInputComponent from '../Components/FormControlInputComponent';
+
+const ProfileView: React.FC = () => {
+  return (
+    <KeyboardAwareScrollView>
+      <IconButton
+        position="absolute"
+        rounded="full"
+        icon={<ArrowBackIcon />}
+        _pressed={{ bgColor: '#00838f80' }}
+      />
+
+      <VStack pt="5" alignItems="center" h="full">
+        <AvatarComponent
+          size="3xs"
+          uri="https://pbs.twimg.com/profile_images/1177303899243343872/B0sUJIH0_400x400.jpg"
+        />
+        <BaseButtonComponent
+          text="Update photo"
+          onPressAction={() => {}}
+          {...{ mt: '4', mb: '10' }}
+        />
+
+        <VStack space="5">
+          <FormControlInputComponent
+            inputName="First name"
+            inputType="string"
+            invalid={true}
+            placeholder="e.g. John"
+            required={true}
+            errorMessage="This field is required"
+          />
+          <FormControlInputComponent
+            inputName="Last name"
+            inputType="string"
+            invalid={false}
+            placeholder="e.g. Wick"
+            required={false}
+          />
+
+          <BaseButtonComponent text="Save" onPressAction={() => {}} />
+        </VStack>
+      </VStack>
+    </KeyboardAwareScrollView>
+  );
+};
+
+export default ProfileView;
