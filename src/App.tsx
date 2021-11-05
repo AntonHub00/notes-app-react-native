185b419df136cc61c37c91d2c67177b1899fac75
Antonio Ochoa <antonhub00@gmail.com>
2021-11-05 05:47:07 -0600

    ADD: Nagivation set in App (entry point)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { NativeBaseProvider, StatusBar } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Views/Home';
+import ProfileView from './Views/ProfileView';
+import TopBarComponent from './Components/TopBarComponent';
+
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
     <NativeBaseProvider>
       <StatusBar backgroundColor="#005662" />
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#00838f',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={({ navigation }) => ({
+              header: props => (
+                <TopBarComponent
+                  onProfileAvatarPress={() => navigation.navigate('Profile')}
+                  {...props}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={ProfileView}
+            options={{ title: 'Profile' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 };
