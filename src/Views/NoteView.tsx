8d83576cab227a775730931fc2ee2dd50769edee
Antonio Ochoa <antonhub00@gmail.com>
2021-11-07 05:20:47 -0600

    ADD: Note view fields

--- src/Views/NoteView.tsx
+++ src/Views/NoteView.tsx
@@ -1,8 +1,49 @@
-import { Text } from 'native-base';
+import { Input, VStack } from 'native-base';
 import React from 'react';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+interface CustomInputProps {
+  placeholder: string;
+  fontSize?: 'sm' | 'lg';
+  extraProps?: Object;
+  onChange?: () => void;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  placeholder,
+  fontSize,
+  onChange,
+  ...extraProps
+}) => {
+  return (
+    <Input
+      placeholder={placeholder}
+      fontSize={fontSize}
+      m="1.5"
+      borderColor="#aaa"
+      selectionColor="#99999980"
+      _focus={{ borderColor: '#00838f', borderWidth: '2' }}
+      numberOfLines={1}
+      multiline={true}
+      onChange={onChange}
+      {...extraProps}
+    />
+  );
+};
 
 const NoteView: React.FC = () => {
-  return <Text>Note view</Text>;
+  return (
+    <KeyboardAwareScrollView>
+      <VStack>
+        <CustomInput
+          placeholder="Title"
+          fontSize="lg"
+          {...{ fontWeight: 'bold' }}
+        />
+        <CustomInput placeholder="Content" fontSize="sm" {...{ mt: '0' }} />
+      </VStack>
+    </KeyboardAwareScrollView>
+  );
 };
 
 export default NoteView;
