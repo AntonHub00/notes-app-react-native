e7457b74f25502d204a71ffb5ccf93e64b212021
Antonio Ochoa <antonhub00@gmail.com>
2021-11-08 05:06:51 -0600

    CHANGE: Validation and state handling in Profile view

--- src/Views/ProfileView.tsx
+++ src/Views/ProfileView.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VStack } from 'native-base';
 import AvatarComponent from '../Components/AvatarComponent';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import BaseButtonComponent from '../Components/BaseButtonComponent';
 import FormControlInputComponent from '../Components/FormControlInputComponent';
 
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { saveProfileInfo } from '../store/stores/profileInfoSlice';
+
 const ProfileView: React.FC = () => {
+  const profileInfoState = useAppSelector(state => state.profileInfo);
+  const actionsDispatcher = useAppDispatch();
+
+  const [firstName, setFirstName] = useState(profileInfoState.firstName);
+  const [lastName, setLastName] = useState(profileInfoState.lastName);
+  const [firstNameInvalid, setFirstNameInvalid] = useState(false);
+
+  const saveCurrentProfileInfo = () => {
+    if (firstName === '') {
+      setFirstNameInvalid(true);
+      return;
+    }
+
+    setFirstNameInvalid(false);
+
+    actionsDispatcher(saveProfileInfo({ firstName, lastName }));
+  };
+
   return (
     <KeyboardAwareScrollView>
       <VStack pt="5" alignItems="center" h="full">
@@ -23,10 +44,12 @@ const ProfileView: React.FC = () => {
           <FormControlInputComponent
             inputName="First name"
             inputType="string"
-            invalid={true}
+            invalid={firstNameInvalid}
             placeholder="e.g. John"
             required={true}
             errorMessage="This field is required"
+            value={firstName}
+            onChangeText={text => setFirstName(text)}
           />
           <FormControlInputComponent
             inputName="Last name"
@@ -34,9 +57,14 @@ const ProfileView: React.FC = () => {
             invalid={false}
             placeholder="e.g. Wick"
             required={false}
+            value={lastName}
+            onChangeText={text => setLastName(text)}
           />
 
-          <BaseButtonComponent text="Save" onPressAction={() => {}} />
+          <BaseButtonComponent
+            text="Save"
+            onPressAction={saveCurrentProfileInfo}
+          />
         </VStack>
       </VStack>
     </KeyboardAwareScrollView>
