639b4d76144e02e93dac93f04234a973bb734fec
Antonio Ochoa <antonhub00@gmail.com>
2021-11-08 16:54:14 -0600

    ADD: Note view fully implemented

--- src/Views/NoteView.tsx
+++ src/Views/NoteView.tsx
@@ -1,20 +1,25 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Input, VStack } from 'native-base';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { RootStackParamList } from '../navigation/CustomNavigatorTypes';
+import uuid from 'react-native-uuid';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { addNote, removeNote, updateNote } from '../store/stores/notesSlice';
 
 interface CustomInputProps {
   placeholder: string;
   fontSize?: 'sm' | 'lg';
   extraProps?: Object;
-  onChange?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
 const CustomInput: React.FC<CustomInputProps> = ({
   placeholder,
   fontSize,
-  onChange,
+  value,
+  onChangeText,
   ...extraProps
 }) => {
   return (
@@ -27,7 +32,8 @@ const CustomInput: React.FC<CustomInputProps> = ({
       _focus={{ borderColor: '#00838f', borderWidth: '2' }}
       numberOfLines={1}
       multiline={true}
-      onChange={onChange}
+      value={value}
+      onChangeText={text => onChangeText?.(text)}
       {...extraProps}
     />
   );
@@ -35,16 +41,90 @@ const CustomInput: React.FC<CustomInputProps> = ({
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Note'>;
 
-const NoteView: React.FC<Props> = () => {
+const NoteView: React.FC<Props> = ({ route }) => {
+  const notesState = useAppSelector(state => state.notes.notes);
+  const actionsDispatcher = useAppDispatch();
+
+  const [noteId, setNoteId] = useState<string>('');
+  const [noteTitle, setnoteTitle] = useState<string>('');
+  const [noteContent, setnoteContent] = useState<string>('');
+
+  const noteIdRef = useRef<string>();
+  noteIdRef.current = noteId;
+
+  const noteTitleRef = useRef<string>();
+  noteTitleRef.current = noteTitle;
+
+  const noteContentRef = useRef<string>();
+  noteContentRef.current = noteContent;
+
+  const updateNoteTitle = (text: string) => {
+    setnoteTitle(text);
+    actionsDispatcher(updateNote({ id: noteId, title: text }));
+  };
+
+  const updateNoteContent = (text: string) => {
+    setnoteContent(text);
+    actionsDispatcher(updateNote({ id: noteId, content: text }));
+  };
+
+  const createNote = () => {
+    const id = String(uuid.v4());
+
+    setNoteId(id);
+
+    actionsDispatcher(addNote({ id, title: '', content: '' }));
+  };
+
+  const setNote = () => {
+    const currentNote = notesState.find(note => note.id === route.params?.id);
+
+    if (currentNote !== undefined) {
+      setNoteId(currentNote.id);
+      setnoteTitle(currentNote.title);
+      setnoteContent(currentNote.content);
+    }
+  };
+
+  const deleteNoteIfEmpty = () => {
+    if (noteTitleRef.current || noteContentRef.current) {
+      return;
+    }
+
+    if (noteIdRef.current !== undefined) {
+      actionsDispatcher(removeNote({ id: noteIdRef.current }));
+    }
+  };
+
+  useEffect(() => {
+    if (route.params?.id === undefined) {
+      createNote();
+    } else {
+      setNote();
+    }
+
+    return deleteNoteIfEmpty;
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <KeyboardAwareScrollView>
       <VStack>
         <CustomInput
           placeholder="Title"
           fontSize="lg"
+          value={noteTitle}
+          onChangeText={text => updateNoteTitle(text)}
           {...{ fontWeight: 'bold' }}
         />
-        <CustomInput placeholder="Content" fontSize="sm" {...{ mt: '0' }} />
+        <CustomInput
+          placeholder="Content"
+          value={noteContent}
+          onChangeText={text => updateNoteContent(text)}
+          fontSize="sm"
+          {...{ mt: '0' }}
+        />
       </VStack>
     </KeyboardAwareScrollView>
   );
