1e95a7ee3a8d7baca534d12d248065dd9ee71360
Antonio Ochoa <antonhub00@gmail.com>
2021-11-08 05:56:14 -0600

    CHANGE: Navigator extracted to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { NativeBaseProvider, StatusBar } from 'native-base';
+
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeView from './Views/HomeView';
-import ProfileView from './Views/ProfileView';
-import TopBarComponent from './Components/TopBarComponent';
-import NoteView from './Views/NoteView';
+import CustomNavigator from './navigation/CustomNavigator';
 
 import store from './store/store';
 import { Provider } from 'react-redux';
-
-const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
@@ -18,40 +13,7 @@ const App: React.FC = () => {
       <NativeBaseProvider>
         <StatusBar backgroundColor="#005662" />
         <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: '#00838f',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-            }}
-          >
-            <Stack.Screen
-              name="Home"
-              component={HomeView}
-              options={({ navigation }) => ({
-                header: props => (
-                  <TopBarComponent
-                    onProfileAvatarPress={() => navigation.navigate('Profile')}
-                    {...props}
-                  />
-                ),
-              })}
-            />
-            <Stack.Screen
-              name="Profile"
-              component={ProfileView}
-              options={{ title: 'Profile' }}
-            />
-            <Stack.Screen
-              name="Note"
-              component={NoteView}
-              options={{ title: 'Note' }}
-            />
-          </Stack.Navigator>
+          <CustomNavigator />
         </NavigationContainer>
       </NativeBaseProvider>
     </Provider>
---
+++ src/navigation/CustomNavigator.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeView from '../Views/HomeView';
+import TopBarComponent from '../Components/TopBarComponent';
+import ProfileView from '../Views/ProfileView';
+import NoteView from '../Views/NoteView';
+
+const Stack = createNativeStackNavigator();
+
+const CustomNavigator: React.FC = () => {
+  const { Navigator, Screen } = Stack;
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#00838f',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={HomeView}
+        options={({ navigation }) => ({
+          header: props => (
+            <TopBarComponent
+              onProfileAvatarPress={() => navigation.navigate('Profile')}
+              {...props}
+            />
+          ),
+        })}
+      />
+      <Screen
+        name="Profile"
+        component={ProfileView}
+        options={{ title: 'Profile' }}
+      />
+      <Screen name="Note" component={NoteView} options={{ title: 'Note' }} />
+    </Navigator>
+  );
+};
+
+export default CustomNavigator;
