9e04c753f892407bab567682ca68ad005e86cb49
Antonio Ochoa <antonhub00@gmail.com>
2021-11-09 04:38:51 -0600

    ADD: Redux persist configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import CustomNavigator from './navigation/CustomNavigator';
 
-import store from './store/store';
+import store, { persistor } from './store/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <NativeBaseProvider>
-        <StatusBar backgroundColor="#005662" />
-        <NavigationContainer>
-          <CustomNavigator />
-        </NavigationContainer>
-      </NativeBaseProvider>
+      <PersistGate persistor={persistor}>
+        <NativeBaseProvider>
+          <StatusBar backgroundColor="#005662" />
+          <NavigationContainer>
+            <CustomNavigator />
+          </NavigationContainer>
+        </NativeBaseProvider>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,51 @@
 import { configureStore } from '@reduxjs/toolkit';
 import notesSliceReducer from './stores/notesSlice';
 import profileInfoSlice from './stores/profileInfoSlice';
+import { combineReducers } from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+// The configuration we want to set to tell redux-persist how we want to persist
+// our state.
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+// Converts all our reducers into a single one.
+const rootReducer = combineReducers({
+  notes: notesSliceReducer,
+  profileInfo: profileInfoSlice,
+});
+
+// Creates a new reducer with all the configuration needed to persist our state.
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    notes: notesSliceReducer,
-    profileInfo: profileInfoSlice,
-  },
+  // We will use the "persistent reducer" (which we configured above) instead of
+  // the ones that we created (slices).
+  reducer: persistedReducer,
+  // The middleware will automatically warn anytime it detects non-serializable
+  // values in our actions or state but, for this case, we want to ignore all the
+  // action types that redux-persist dispatches.
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 // Exports the root state/store so in can be passed to the provider in the
 // "root" component.
