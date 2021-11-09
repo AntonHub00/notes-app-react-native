a05d24b22787c31f7da9218b7bb90c659f4fa0f6
Antonio Ochoa <antonhub00@gmail.com>
2021-11-09 07:27:00 -0600

    CHANGE: Now avatar component accepts any props

--- src/Components/AvatarComponent.tsx
+++ src/Components/AvatarComponent.tsx
@@ -5,10 +5,11 @@ import { IAvatarProps } from 'native-base/lib/typescript/components/composites/A
 interface Props {
   size: IAvatarProps['size'];
   source: IAvatarProps['source'];
+  extraProps?: Object;
 }
 
-const AvatarComponent: React.FC<Props> = ({ size, source }) => {
-  return <Avatar size={size} source={source} />;
+const AvatarComponent: React.FC<Props> = ({ size, source, ...extraProps }) => {
+  return <Avatar size={size} source={source} {...extraProps} />;
 };
 
 export default AvatarComponent;
